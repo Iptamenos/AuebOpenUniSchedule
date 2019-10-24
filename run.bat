e153d438629dc0cbc837e3c334eea652c3686b1c
Christos Kormaris <chriskormaris@gmail.com>
2019-10-24 16:53:57 +0300

    Change cmd commands for Windows.

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 chcp 1253
-java -cp .;bin;libs\commons-io-2.5.jar;libs\java-json.jar;libs\jxl-2.6.12.jar Main -Dfile.encoding=UTF-8
+java -cp .;bin;libs\commons-io-2.5.jar;libs\java-json.jar;libs\jxl-2.6.12.jar auebunischedule.Main -Dfile.encoding=UTF-8
 pause;
