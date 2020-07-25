78c6878af0ad4b72b193db27873f77e25c9be31f
Christos Kormaris <chriskormaris@gmail.com>
2020-07-25 18:45:18 +0300

    Update the `pom.xml` file. Delete the compile and run `.bat` files.

--- compileAll.bat
+++
@@ -1,3 +0,0 @@
-mkdir bin
-javac -cp .;lib\commons-io-2.5.jar;lib\java-json.jar;lib\jxl-2.6.12.jar src\auebunischedule\*.java -d bin -encoding utf-8
-pause
--- run.bat
+++
@@ -1,9 +0,0 @@
-chcp 1253
-java -cp bin;lib\commons-io-2.5.jar;lib\java-json.jar;lib\jxl-2.6.12.jar auebunischedule.Main -Dfile.encoding=UTF-8
-
-:: Copy the file "xslt\schedules.xsl" to the folder "schedules\xml".
-if not exist schedules mkdir schedules
-if not exist schedules\xml mkdir schedules\xml
-copy xslt\schedules.xsl schedules\xml
-
-pause
