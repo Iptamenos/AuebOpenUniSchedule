48fd62f4e3075afbaaec9a83c585c9f3d2cf36ca
Christos Kormaris <chriskormaris@gmail.com>
2020-07-25 15:50:19 +0300

    Update the `.gitignore` file. Update the `run.bat` file. Delete old produced schedule files. Unstage the `lib` folder files.

--- run.bat
+++ run.bat
@@ -1,3 +1,9 @@
 chcp 1253
-java -cp .;bin;libs\commons-io-2.5.jar;libs\java-json.jar;libs\jxl-2.6.12.jar auebunischedule.Main -Dfile.encoding=UTF-8
+java -cp .;bin;lib\commons-io-2.5.jar;lib\java-json.jar;lib\jxl-2.6.12.jar auebunischedule.Main -Dfile.encoding=UTF-8
+
+:: Copy the file "xslt\schedules.xsl" to the folder "schedules\xml".
+if not exist schedules mkdir schedules
+if not exist schedules\xml mkdir schedules\xml
+copy xslt\schedules.xsl schedules\xml
+
 pause;
